1ec272b9fce1882cdd88ebc150adaed57587b310
syllomex <leonardojdossantos@hotmail.com>
2020-09-01 08:11:39 -0300

    Configura banco de dados

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+require("./services/database")();
+
 app.use(cors());
 app.use(express.json());
 
---
+++ server/src/services/database.js
@@ -0,0 +1,13 @@
+const mongo = require("mongoose");
+
+module.exports = async () => {
+  try {
+    await mongo.connect("mongodb://localhost:27017/dinamica_teste", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected to mongodb");
+  } catch (error) {
+    console.log("error on mongodb connection:", error.message);
+  }
+};
